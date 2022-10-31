8d8471ba8dbb7a40d49275f76ced69b14655e3a5
Pavlo Vovzhynskyy <pavlovovzhynskyy@gmail.com>
2022-10-31 18:09:54 +0100

    add rrd & tested feature with emotion

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,30 +1,14 @@
-/** @jsxImportSource theme-ui */
-
 import { useColorMode } from 'theme-ui';
+import { Input } from './App.styled';
 
 const App = () => {
 	const [colorMode, setColorMode] = useColorMode();
 	return (
-		<div
-			sx={{
-				height: '100vh',
-				display: 'flex',
-				justifyContent: 'center',
-				alignItems: 'center',
-				fontSize: 40,
-			}}
-		>
-			React homework template
-			<input
-				type="checkbox"
-				checked={colorMode === 'light' ? true : false}
-				onChange={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}
-				sx={{
-					bg: 'primary',
-					padding: '10px 20px',
-				}}
-			/>
-		</div>
+		<Input
+			type="checkbox"
+			checked={colorMode === 'light' ? true : false}
+			onChange={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}
+		/>
 	);
 };
 
